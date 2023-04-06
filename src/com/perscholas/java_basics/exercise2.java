a91848ebf49009b088c14bf3e6c25446bc367f2c
diroy41 <diroy41@hotmail.com>
2023-04-06 10:34:37 -0500

    Created second exercise

---
+++ src/com/perscholas/java_basics/exercise2.java
@@ -0,0 +1,11 @@
+package com.perscholas.java_basics;
+
+public class exercise2 {
+    public static void main(String[] args) {
+        double fvar = 2.2;
+        double Svar = 4.2;
+        double sum = fvar+Svar;
+
+        System.out.println(sum);
+    }
+}
