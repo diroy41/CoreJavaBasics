0f436167ca27b8634220b39286f321c77c360a1b
diroy41 <diroy41@hotmail.com>
2023-04-06 10:21:43 -0500

    Created first exercise1

---
+++ src/com/perscholas/java_basics/exercise1.java
@@ -0,0 +1,12 @@
+package com.perscholas.java_basics;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        int firstint = 1;
+        int secondint = 2;
+        int sum = firstint+secondint;
+
+        System.out.println(sum);
+
+    }
+}
