49f58611d9fc0d0e9bc023396a0d69ec780f98d4
diroy41 <diroy41@hotmail.com>
2023-04-06 10:42:48 -0500

    Coded third exercise

--- src/com/perscholas/java_basics/exercise3.java
+++ src/com/perscholas/java_basics/exercise3.java
@@ -1,4 +1,13 @@
 package com.perscholas.java_basics;
 
 public class exercise3 {
+
+    public static void main(String[] args) {
+        int fvar = 2;
+        double svar = 2.5;
+        double sum = fvar + svar;
+
+        System.out.println(sum);
+
+    }
 }
